6f8b1e0319d396c867e8d09cb4b8742e227871c8
RahulRanjann <rahul1913111054@gmail.com>
2022-10-09 13:05:34 +0530

    c++ basics

---
+++ Sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num =10;
+    int num2 = 20;
+    cout <<"The sum of the two number is" << num+num2;
+    return 0;
+}
---
+++ Variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int num = 10;
+    char character = 'R';
+    string name = "Rahul";
+    float fl = 20.4;
+    cout << "name:"<<name <<"\nage :" <<num <<"\nmy first letter :"<<character;
+
+    
+    return 0;
+}
