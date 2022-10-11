9e024b11d43e979ac31d0e4a06b1a0a70b4ecf5d
RahulRanjann <rahul1913111054@gmail.com>
2022-10-11 08:56:00 +0530

    right traingle

---
+++ Right.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Ente the no of row \n ";
+    cin >> n;
+    for (int  i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";   
+    }
+  return 0;  
+}
